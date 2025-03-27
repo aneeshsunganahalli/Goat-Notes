b74bc369c4b7b495e46e05443a19aea786059ebd
Aneesh <aneeshappid@gmail.com>
2025-03-27 12:13:55 +0530

    Fixed Display Name Issue

--- src/components/AskAIButton.tsx
+++ src/components/AskAIButton.tsx
@@ -26,22 +26,27 @@ type Props = {
 const WideDialogContent = dynamic(
   () => 
     Promise.resolve(
-      React.forwardRef<
-        React.ElementRef<typeof DialogPrimitive.Content>,
-        React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
-      >(({ className, children, ...props }, ref) => (
-        <DialogPrimitive.Portal>
-          <DialogPrimitive.Overlay className="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
-          <DialogPrimitive.Content
-            ref={ref}
-            className={`fixed left-[50%] top-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] gap-4 rounded-xl border bg-background shadow-xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] ${className}`}
-            style={{ width: '92vw', maxWidth: '960px', height: '82vh' }}
-            {...props}
-          >
-            {children}
-          </DialogPrimitive.Content>
-        </DialogPrimitive.Portal>
-      ))
+      (() => {
+        const DialogContentComponent = React.forwardRef<
+          React.ElementRef<typeof DialogPrimitive.Content>,
+          React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+        >(({ className, children, ...props }, ref) => (
+          <DialogPrimitive.Portal>
+            <DialogPrimitive.Overlay className="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+            <DialogPrimitive.Content
+              ref={ref}
+              className={`fixed left-[50%] top-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] gap-4 rounded-xl border bg-background shadow-xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] ${className}`}
+              style={{ width: '92vw', maxWidth: '960px', height: '82vh' }}
+              {...props}
+            >
+              {children}
+            </DialogPrimitive.Content>
+          </DialogPrimitive.Portal>
+        ));
+        
+        DialogContentComponent.displayName = "DialogContentComponent";
+        return DialogContentComponent;
+      })()
     ),
   { ssr: false }
 );
